417cd58c0c01fc1bce33633aa6b731cdb4bbbbdf
avengestaves <avengestaves@proton.me>
2024-10-04 18:40:05 -0400

    Now it counts characters too

--- main.py
+++ main.py
@@ -2,17 +2,34 @@ def main():
     file = "books/frankenstein.txt"
     output_of_file = output_complete_file(file)
     wordcount_of_file = count_words(output_of_file)
-    print(output_of_file)
+    character_count = count_characters(output_of_file)
+    #print(output_of_file)
     print(wordcount_of_file)
+    print(character_count)
 
-def count_words(list):
-    file_as_list = list.split()
-    length_of_list = len(file_as_list)
+def split_to_list(file):
+    return file.split()
+
+def count_words(file):
+    length_of_list = len(split_to_list(file))
     return length_of_list
 
 def output_complete_file(book):
     with open(book) as f:
         file_output = f.read()
     return file_output
-    
+
+def count_characters(file):
+    char_count_dict = {}
+    #for item in range(0, 5):
+    for item in split_to_list(file):
+        #lowered = item.lower()
+        #print(lowered)
+        for char in item.lower():
+            if char in char_count_dict:
+                char_count_dict[char] += 1
+            else:
+                char_count_dict[char] = 1
+    return char_count_dict
+
 main()
